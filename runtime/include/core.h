d6f6c8007d5d22f1df4fdd150461f6e61683e5ea
Bryan DeGrendel <bryan@degrendel.com>
2014-03-12 21:51:15 -0400

    Wrote a simple Core class for the runtime.
    1. Will wrap around an initialized instance of the runtime.

---
+++ runtime/include/core.h
@@ -0,0 +1,26 @@
+// StoryMaster Runtime
+// Bryan DeGrendel (c) 2014
+//
+// See LICENSE for licensing information.
+//
+// core.h - Class wrapping around an initialized instance of the runtime
+
+#ifndef ___SM_CORE_H___
+#define ___SM_CORE_H___
+
+namespace sm
+{
+
+class Core
+{
+public:
+  Core();
+  ~Core();
+
+private:
+};
+
+}
+
+#endif//___SM_CORE_H___
+
---
+++ runtime/source/core.cpp
@@ -0,0 +1,17 @@
+// StoryMaster Runtime
+// Bryan DeGrendel (c) 2014
+//
+// See LICENSE for licensing information.
+//
+// core.cpp - Class wrapping around an initialized instance of the runtime
+
+#include "core.h"
+
+sm::Core::Core()
+{
+}
+
+sm::Core::~Core()
+{
+}
+
