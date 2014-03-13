eb4631a670293b9234af053c55a793bb267b5422
Bryan DeGrendel <bryan@degrendel.com>
2014-03-12 22:22:02 -0400

    Added a simple start to an example simulator.
    1. Currently just includes and creates the core objects.

---
+++ simulator/source/simulator.cpp
@@ -0,0 +1,20 @@
+// StoryMaster Simulator
+// Bryan DeGrendel (c) 2014
+//
+// See LICENSE for licensing information.
+//
+// simulator.cpp - Main file for the sample StoryMaster simulator.
+
+#include <cstdio>
+
+#include <iostream>
+
+#include <storymaster.h>
+
+int main(int argc, const char *argv[])
+{
+  sm::Core core;
+  sm::Scene scene(&core);
+  return 0;
+}
+
