75445f80b2c25964a2f4942625f5f668a9400994
Bryan DeGrendel <bryan@degrendel.com>
2014-03-14 15:56:04 -0400

    Simulator now malloc/frees all memory.

--- simulator/source/simulator.cpp
+++ simulator/source/simulator.cpp
@@ -5,6 +5,7 @@
 //
 // simulator.cpp - Main file for the sample StoryMaster simulator.
 
+#include <cstdlib>
 #include <cstdio>
 
 #include <iostream>
@@ -13,11 +14,19 @@
 
 int main(int argc, const char *argv[])
 {
-  sm_core core;
-  sm_scene scene;
+  sm_core *core = (sm_core *)(malloc(sizeof(sm_core)));
+  sm_scene *scene = (sm_scene *)(malloc(sizeof(sm_scene)));
 
-  sm_core_init(&core);
-  sm_scene_init(&scene, &core, "examples/dialog.scene.lua", "dialog");
+  sm_core_init(core);
+  sm_scene_init(scene, core, "examples/dialog.scene.lua", "dialog");
+
+  // ...
+
+  sm_scene_deinit(scene);
+  sm_core_deinit(core);
+
+  free(scene);
+  free(core);
 
   return 0;
 }
