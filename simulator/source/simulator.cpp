24829ca1b4c54b40a70137c7a140ac429ac149b7
Bryan DeGrendel <bryan@degrendel.com>
2014-03-17 14:55:28 -0400

    Updated the simulator to the latest API.

--- simulator/source/simulator.cpp
+++ simulator/source/simulator.cpp
@@ -19,15 +19,19 @@ int main(int argc, const char *argv[])
   cout << "Operating over [examples/dialog.scene.lua] with runtime " SM_VERSION_STRING << endl;
 
   sm_core *core = (sm_core *)(malloc(sizeof(sm_core)));
+  sm_session *session = (sm_session *)(malloc(sizeof(sm_session)));
   sm_scene *scene = (sm_scene *)(malloc(sizeof(sm_scene)));
 
   // TODO: Check return valuez and all that
   sm_core_init(core);
-  sm_scene_init(scene, core, "dialog");
+  sm_session_init(session, core, "common");
+  sm_scene_init(scene, session, "dialog");
 
   sm_scene_load_from_file(scene, "examples/dialog.scene.lua");
+  sm_scene_execute(scene);
 
   sm_scene_deinit(scene);
+  sm_session_deinit(session);
   sm_core_deinit(core);
 
   free(scene);
