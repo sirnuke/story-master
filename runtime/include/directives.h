892391ab0749fd6e83f7a639db98f30e079a5093
Bryan DeGrendel <bryan@degrendel.com>
2014-03-17 00:56:53 -0400

    Added beginning of directives implementation.

---
+++ runtime/include/directives.h
@@ -0,0 +1,17 @@
+// StoryMaster Runtime
+// Bryan DeGrendel (c) 2014
+//
+// See LICENSE for licensing information.
+//
+// directives.h - Implements basic Lua directive callbacks
+
+#ifndef ___SM_DIRECTIVES_H___
+#define ___SM_DIRECTIVES_H___
+
+#include "types.h"
+
+struct lua_State;
+
+int _sm_directive_dialog(struct lua_State *lua);
+
+#endif//___SM_DIRECTIVES_H___
---
+++ runtime/source/directives.c
@@ -0,0 +1,18 @@
+// StoryMaster Runtime
+// Bryan DeGrendel (c) 2014
+//
+// See LICENSE for licensing information.
+//
+// directives.cpp - Implements basic Lua directive callbacks
+
+#include <assert.h>
+
+#include "directives.h"
+
+int _sm_directive_dialog(struct lua_State *lua)
+{
+  assert(lua);
+
+  return 0;
+}
+
