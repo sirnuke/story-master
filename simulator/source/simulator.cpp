c0318a578810d51cae6a2b23448f75412395ac1d
Bryan DeGrendel <bryan@degrendel.com>
2014-03-17 17:34:02 -0400

    Fixed pathnames in simulator.

--- simulator/source/simulator.cpp
+++ simulator/source/simulator.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
-  cout << "Operating over [examples/dialog.scene.lua] with runtime " SM_VERSION_STRING << endl;
+  cout << "Operating over [examples/dialog.lua] with runtime " SM_VERSION_STRING << endl;
 
   sm_core *core = (sm_core *)(malloc(sizeof(sm_core)));
   sm_session *session = (sm_session *)(malloc(sizeof(sm_session)));
@@ -25,7 +25,7 @@ int main(int argc, const char *argv[])
   // TODO: Check return valuez and all that
   sm_core_init(core);
   sm_session_init(session, core, "common");
-  sm_scene_init_from_file(scene, session, "dialog", "example/dialog.lua");
+  sm_scene_init_from_file(scene, session, "dialog", "examples/dialog.lua");
 
   sm_scene_execute(scene);
 
